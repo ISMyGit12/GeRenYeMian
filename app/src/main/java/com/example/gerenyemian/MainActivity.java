837bba69052d6a0d71908ca1602a674452796564
ISMyGit12 <495622061@qq.com>
2021-04-24 22:46:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/gerenyemian/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gerenyemian;
+//190470122 渠立伟
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
